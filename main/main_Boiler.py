0481b80b08ffe8543fedb3690cbc6312c5f63f20
Gang Hu <953773533@qq.com>
2025-11-10 09:21:41 +0800

    修改了Boiler文件，保证能够运行。

--- main/main_Boiler.py
+++ main/main_Boiler.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 
 if __name__ == '__main__':
-    all_files = os.listdir(os.path.join('../../datasets', 'Boiler'))
+    all_files = os.listdir(os.path.join('datasets', 'Boiler'))
 
     files = [name[:-4] for name in all_files if name.endswith('.csv')]
 
@@ -12,13 +12,13 @@ if __name__ == '__main__':
             if src != trg:
                 print('src: ', src, ' / target: ', trg)
 
-                train = os.path.join('[path-to-dacad]/main/', 'train.py')
-                command = ['python', train, '--algo_name', 'cluda', '--num_epochs', '20', '--queue_size', '98304', '--momentum', '0.99', '--batch_size', '256',
+                train = os.path.join('main', 'train.py')
+                command = ['python', train, '--algo_name', 'dacad', '--num_epochs', '20', '--queue_size', '98304', '--momentum', '0.99', '--batch_size', '256',
                            '--eval_batch_size', '256', '--learning_rate', '1e-4', '--dropout', '0.2', '--weight_decay', '1e-4', '--num_channels_TCN', '128-128-128',
-                           '--dilation_factor_TCN', '3', '--kernel_size_TCN', '7', '--hidden_dim_MLP', '256', '--id_src', src, '--id_trg', trg,
-                           '--experiment_folder', 'boiler']
+                           '--dilation_factor_TCN', '3', '--kernel_size_TCN', '7', '--hidden_dim_MLP', '256', '--path_src', 'datasets/Boiler', '--path_trg', 'datasets/Boiler',
+                           '--id_src', src, '--id_trg', trg, '--experiment_folder', 'boiler']
                 subprocess.run(command)
 
-                test = os.path.join('[path-to-dacad]/main/', 'eval.py')
+                test = os.path.join('main', 'eval.py')
                 command1 = ['python', test, '--experiments_main_folder', 'results', '--experiment_folder', 'boiler', '--id_src', src, '--id_trg', trg]
                 subprocess.run(command1)
